a6ea08bf7a3b9189d581487a67d2f0de71b60aea
KelvynLenis <kmartinslenis30@hotmail.com>
2022-06-25 15:15:31 -0300

    changing idea

--- main.cpp
+++ main.cpp
@@ -12,135 +12,6 @@ using namespace std;
 static long num_steps = 5;
 const int sz = 1000000;
 
-void swap(int* a, int* b){
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
- 
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-int partition (int arr[], int low, int high){
-	int pivot = arr[high]; // pivot
-	int i = (low - 1); // Index of smaller element and indicates the right position of pivot found so far
-
-	for (int j = low; j <= high - 1; j++){
-		// If current element is smaller than the pivot
-		if (arr[j] < pivot){
-			i++; // increment index of smaller element
-			swap(&arr[i], &arr[j]);
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-	return (i + 1);
-}
- 
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(int arr[], int low, int high){
-	if (low < high){
-		#pragma omp parallel shared(arr, low, high)
-		{
-			/* pi is partitioning index, arr[p] is now
-			at right place */
-			int pi = partition(arr, low, high);
-
-			// Separately sort elements before
-			// partition and after partition
-			#pragma omp
-			quickSort(arr, low, pi - 1);
-			#pragma omp
-			quickSort(arr, pi + 1, high);
-		}
-	}
-}
-
-void merge(int array[], int const left, int const mid, int const right){
-	auto const subArrayOne = mid - left + 1;
-	auto const subArrayTwo = right - mid;
-
-	// Create temp arrays
-	auto *leftArray = new int[subArrayOne],
-				*rightArray = new int[subArrayTwo];
-
-	// Copy data to temp arrays leftArray[] and rightArray[]
-	#pragma omp parallel shared(array)
-	{
-		#pragma omp for nowait
-		for (auto i = 0; i < subArrayOne; i++){
-			leftArray[i] = array[left + i];
-		}
-
-		#pragma omp for nowait
-		for (auto j = 0; j < subArrayTwo; j++){
-			rightArray[j] = array[mid + 1 + j];
-		}
- 
-		auto indexOfSubArrayOne = 0; // Initial index of first sub-array
-		auto indexOfSubArrayTwo = 0; // Initial index of second sub-array
-		int indexOfMergedArray = left; // Initial index of merged array
-
-		// Merge the temp arrays back into array[left..right]
-		#pragma omp while
-		while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo) {
-			if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
-					array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
-					indexOfSubArrayOne++;
-			}
-			else {
-					array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
-					indexOfSubArrayTwo++;
-			}
-			indexOfMergedArray++;
-		}
-		// Copy the remaining elements of
-		// left[], if there are any
-		#pragma omp while nowait shared(indexOfMergedArray)
-		while (indexOfSubArrayOne < subArrayOne) {
-			array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
-			indexOfSubArrayOne++;
-			indexOfMergedArray++;
-		}
-		// Copy the remaining elements of
-		// right[], if there are any
-		#pragma omp while nowait shared(indexOfMergedArray)
-		while (indexOfSubArrayTwo < subArrayTwo) {
-			array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
-			indexOfSubArrayTwo++;
-			indexOfMergedArray++;
-		}
-	}
-}
-
-void mergeSort(int array[], int const begin, int const end){
-	if (begin >= end)
-			return; // Returns recursively
-
-	auto mid = begin + (end - begin) / 2;
-
-	// Com paralelismo
-	#pragma omp parallel shared(array, mid, begin, end)
-	{
-		#pragma omp nowait
-		mergeSort(array, begin, mid);
-		#pragma omp nowait
-		mergeSort(array, mid + 1, end);
-		#pragma omp nowait
-		merge(array, begin, mid, end);
-	}
-
-	//  Sem paralelismo
-	// mergeSort(array, begin, mid);
-	// mergeSort(array, mid + 1, end);
-	// merge(array, begin, mid, end);
-
-}
-
 int main() {
 
 	srand((unsigned) time(0));
@@ -163,14 +34,14 @@ int main() {
 	// ================ Ordenação do array ==================
 	auto t1 = std::chrono::high_resolution_clock::now();
 
-	for(int i = 0; i < num_steps; i++){
-		// mergeSort(myArr, 0, arr_size - 1);
-		quickSort(myArr, 0, arr_size -1);
+	#pragma omp parallel for
+	for(int i=0;i<sz;i++){
+		myArr[i] = rand()%100;
 	}
 
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto duration = (std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count());
 	float time = (float)duration/1000000;
 
-	std::cout << "Tempo de processamento = " << time << " segundos." << std::endl;
+	std::cout << "Init Parallel: " << time << " microsegundos." << std::endl;
 }
--- search.cpp
+++ search.cpp
@@ -7,102 +7,95 @@
 #include <vector>
 
 #define NUM_THREADS 4
+#define PAD 8  // assume 64 byte L1 cache line size
 
 using namespace std;
 static long num_steps = 100;
-static const long sz = 100000;
+static const long sz = 1000000;
 int nthreads;
 
 int search(int arr[], int value){
   for(int i = 0; i < sz; i++){
     if(i == value){
-      return i;
+      // cout << i << endl;
+      return 1;
     }
   }
 
   return 0;
 }
 
-void searchParallel(int arr[], int value){
-  int slice1[sz / 4];
-  int slice2[sz / 4];
-  int slice3[sz / 4];
-  int slice4[sz / 4];
-
-  int first = sz * 0.25;
-  int second = sz * 0.50;
-  int third = sz * 0.75;
-
-  #pragma omp parallel sections shared(arr)
+void parallel_search (int slice1[], int slice2[], int value) {
+  #pragma omp parallel
   {
-    #pragma omp section
-    {
-      for(int i = 0; i < first; i++){
-        slice1[i] = arr[i];
-      }
+    int nthd=omp_get_thread_num();
+    
+    //check whether cancellation has been activated before proceed
+    #pragma omp cancellation point parallel
+    bool found = search(slice1, value);
+  
+    if(found){
+        //cancel the parallel construct
+        #pragma omp cancel parallel
     }
-    #pragma omp section
-    {
-      for(int j = first; j < second; j++){
-        slice2[j] = arr[j];    
+
+    //check whether cancellation has been activated before proceed
+    #pragma omp cancellation point parallel
+
+    //If the first level search is not successful, go on to the second level search
+    search(slice2, value); 
+  }
+}
+
+void searchParallel(int arr[], int value){
+  int i;
+  #pragma omp parallel default(none) shared(arr, value) private(i)
+  {
+    #pragma omp for nowait
+    for(i = 0; i < sz/2; i++){
+      if (arr[i] == value)
+      {
+        /* code */
       }
+      
     }
-    // #pragma omp for
-    // for(int k = second; k < third; k++){
-    //   slice3[k] = arr[k];
+
+    #pragma omp for nowait
+    for(i = sz/2; i < sz; i++){
+      if (arr[i] == value)
+      {
+        /* code */
+      }
+
+    }
+  }
+}
+
+void initArray(int arr[]){
+  omp_set_num_threads(NUM_THREADS);
+
+  int i;
+
+  #pragma omp parallel default(none) shared(arr) private(i)
+  {
+    // #pragma omp for nowait
+    for(i = 0; i < sz; i++){
+      arr[i] =  rand()%100;
+    }
+    
+    // #pragma omp for nowait
+    // for(i = first; i < second; j++){
+    //   arr[i] =  rand()%100;    
     // }
-    // #pragma omp for
-    // for(int l = third; l < sz; l++){
-    //   slice4[l] = arr[l];
+    // #pragma omp for nowait
+    // for(i = second; i < third; k++){
+    //   arr[i] =  rand()%100;
+    // }
+    // #pragma omp for nowait
+    // for(i = third; i < sz; l++){
+    //   arr[i] =  rand()%100;
     // }
   }
-
-  // #pragma omp parallel for shared(arr)
-  // for(int i = 0; i < first; i++){
-  //   slice1[i] = arr[i];
-  // }
-  // for(int j = first; j < second; j++){
-  //   slice2[j] = arr[j];    
-  // }
-  // for(int k = second; k < third; k++){
-  //   slice3[k] = arr[k];
-  // }
-  // for(int l = third; l < sz; l++){
-  //   slice4[l] = arr[l];
-  // }
-
-  // #pragma omp parallel shared(arr)
-  // {
-  //   // busca
-  //   #pragma omp for nowait
-  //   for(int i = 0; i < first; i++){
-  //     if(arr[i] == value){
-  //       // cout <<  i << endl;
-  //       #pragma omp cancel for
-  //     }
-  //   }
-  //   #pragma omp for nowait
-  //   for(int j = first; j < second; j++){
-  //     if(arr[j] == value){
-  //       // cout << j << endl;
-  //       #pragma omp cancel for
-  //     }    
-  //   }
-  //   #pragma omp for nowait
-  //   for(int k = second; k < third; k++){
-  //     if(arr[k] == value){
-  //       // cout << k << endl;
-  //       #pragma omp cancel for
-  //     }
-  //   }
-  //   #pragma omp for nowait
-  //   for(int l = third; l < sz; l++){
-  //     if(arr[l] == value){
-  //       // cout << l << endl;
-  //       #pragma omp cancel for
-  //     }
-  //   }
-  // }
 }
 
 vector<int> init(){
@@ -117,15 +110,19 @@ vector<int> init(){
 vector<int> initParallel(){
   vector<int> vec(sz, 0);
 
-  #pragma omp parallel shared(vec)
+  #pragma omp parallel // shared(vec)
   {
-    #pragma omp for
+    #pragma omp for nowait
     for(int i = 0; i < (sz / 2); i++) {
       vec[i] = rand()%100;
     }
-    #pragma omp for 
-    for(int j = (sz / 2); j < sz; j++){
+    #pragma omp for nowait
+    for(int j = (sz / 2); j < (sz / 3); j++){
       vec[j] = rand()%100;
+    }
+    #pragma omp for nowait
+    for(int k = (sz / 3); k < sz; k++){
+      vec[k] = rand()%100;
     }
   } 
 
@@ -142,11 +139,47 @@ int searchVector(vector<int> vec, int value){
 }
 
 int searchVectorParallel(vector<int> vec, int value){
-  #pragma omp parallel
-  {
-    for(auto i : vec){
-      if(i == value){
-      }
+  // int first = sz * 0.25;
+  // int second = sz * 0.50;
+  // int third = sz * 0.75;
+
+  // vector<int> firstPart;
+  // vector<int> secondPart;
+  // vector<int> thirdPart;
+  // vector<int> fourthPart;
+
+  // copy(vec.begin(), vec.begin() + first, firstPart);
+  // copy(vec.begin() + first, vec.begin() + second, secondPart);
+  // copy(vec.begin() + second, vec.begin() + third, thirdPart);
+  // copy(vec.begin() + third, vec.end(), fourthPart);
+
+  // #pragma omp parallel
+  // {
+  //   #pragma omp for nowait 
+  //   for(auto i : firstPart){
+  //     if(i == value){
+  //     }
+  //   }
+  //   #pragma omp for nowait
+  //   for(auto j : secondPart){
+  //     if(j == value){
+  //     }
+  //   }
+  //   #pragma omp for nowait
+  //   for(auto k : thirdPart){
+  //     if(k == value){
+  //     }
+  //   }
+  //   #pragma omp for nowait
+  //   for(auto l : fourthPart){
+  //     if(l == value){
+  //     }
+  //   }
+  // }
+
+  for(auto x : vec){
+    if(x == value){
+      return x;
     }
   }
   return value;
@@ -174,31 +207,41 @@ int main() {
 	float timeInit = (float)durationInit/1000000;
 	cout << "Init Array time: " << timeInit << endl;
 
+  // ============= Inicialização paralela do array =============
+	auto tinitParallelArray = std::chrono::high_resolution_clock::now();
+
+	initArray(myArr);
+
+	auto tinitParallelArray2 = std::chrono::high_resolution_clock::now();
+	auto durationInitParallelArray = (chrono::duration_cast<chrono::microseconds>( tinitParallelArray2 - tinitParallelArray ).count());
+	float timeInitParallelArray = (float)durationInitParallelArray/1000000;
+	cout << "Init Parallel Array time: " << timeInitParallelArray << endl;
+
   // ================== Inicialização do Vetor ==================
 
-  auto tinitVector = std::chrono::high_resolution_clock::now();
+  // auto tinitVector = std::chrono::high_resolution_clock::now();
 
-  vector<int> sequential;
-  for(int i = 0; i < num_steps; i++){
-    sequential = init();
-  }
+  // vector<int> sequential;
+  // for(int i = 0; i < num_steps; i++){
+  //   sequential = init();
+  // }
 
-	auto tinitVector2 = std::chrono::high_resolution_clock::now();
-	auto durationInitVector = (chrono::duration_cast<chrono::microseconds>( tinitVector2 - tinitVector ).count());
-	float timeInitVector = (float)durationInitVector/1000000;
-	cout << "Init Vector sequential time: " << timeInit << endl;
+	// auto tinitVector2 = std::chrono::high_resolution_clock::now();
+	// auto durationInitVector = (chrono::duration_cast<chrono::microseconds>( tinitVector2 - tinitVector ).count());
+	// float timeInitVector = (float)durationInitVector/1000000;
+	// cout << "Init Vector sequential time: " << timeInitVector << endl;
 
 	// ========= Inicialização do Vetor paralela ==================
 
-  auto tinitParallel = std::chrono::high_resolution_clock::now();
+  // auto tinitParallel = std::chrono::high_resolution_clock::now();
 
-  vector<int> parallel;
-  parallel = initParallel();
+  // vector<int> parallel;
+  // parallel = initParallel();
 
-	auto tinitParallel2 = std::chrono::high_resolution_clock::now();
-	auto durationInitParallel = (chrono::duration_cast<chrono::microseconds>( tinitParallel2 - tinitParallel ).count());
-	float timeInitParallel = (float)durationInitParallel/1000000;
-	cout << "Init Vector Parallel time: " << timeInit << endl;
+	// auto tinitParallel2 = std::chrono::high_resolution_clock::now();
+	// auto durationInitParallel = (chrono::duration_cast<chrono::microseconds>( tinitParallel2 - tinitParallel ).count());
+	// float timeInitParallel = (float)durationInitParallel/1000000;
+	// cout << "Init Vector Parallel time: " << timeInitParallel << endl;
   
 
 	// ================ Busca no array ==================
@@ -206,8 +249,8 @@ int main() {
 
 	for(int i = 0; i < num_steps; i++){
     search(myArr, 7);
-	}
   // searchVector(sequential, 8);
+	}
 
 	auto t2 = std::chrono::high_resolution_clock::now();
 	auto duration = (std::chrono::duration_cast<std::chrono::nanoseconds>( t2 - t1 ).count());
@@ -220,14 +263,14 @@ int main() {
 	auto tParallel1 = std::chrono::high_resolution_clock::now();
 
 	for(int i = 0; i < num_steps; i++){
-    // searchParallel(myArr, 7);
-    searchVectorParallel(parallel, 7);
+    searchParallel(myArr, 7);
+    // searchVectorParallel(parallel, 7);
 	}
 
 	auto tParallel2 = std::chrono::high_resolution_clock::now();
 	auto durationParallel = (std::chrono::duration_cast<std::chrono::nanoseconds>( tParallel2 - tParallel1 ).count());
 	float timeParallel = (float)durationParallel/1000000;
 
-	std::cout << "Tempo de processamento paralelo = " << time << " segundos." << std::endl;
+	std::cout << "Tempo de Busca paralela = " << timeParallel << " segundos." << std::endl;
 
 }
